7f42e09cfa6d257addf96393c1762f7af34993e8
Marcos Vinicius <marcosoliacc@gmail.com>
2023-10-24 14:58:45 -0300

    Styles of initial page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import styles from '../styles/HomeNoAuth.module.scss'
-
+import HeaderNoAuth from '../components/homeNoAuth/headerNoAuth'
 export default function HomeNoAuth() {
   return(
     <>
       <main>
-        
+        <HeaderNoAuth/>
       </main>
     </>
   )
---
+++ src/components/homeNoAuth/headerNoAuth/index.tsx
@@ -0,0 +1,24 @@
+/* eslint-disable @next/next/no-img-element */
+import Link from 'next/link'
+import styles from './style.module.scss'
+import { Container, Button } from 'reactstrap'
+const HeaderNoAuth = ()=> {
+    return(
+        <>
+            <div className={styles.ctaSection}>
+                <img src="/homeNoAuth/logoCta.png" alt="logoCta" className={styles.imgCta}/>
+                <p>Se cadastre para ter acesso aos cursos</p>
+                <img src="/homeNoAuth/logoCta.png" alt="logoCta" className={styles.imgCta}/>
+            </div>
+            <section className={styles.nav}>
+                <img src="/logoOnebitflix.svg" alt="logo da OneBitFlix" className={styles.imgNav}/>
+                <div className={styles.divBnts}>
+                    <Link href='/login'><button className={styles.navBtn}>Entrar</button></Link>
+                    <Link href='/register'><button className={styles.navBtn}>Quero fazer parte</button></Link>
+                    
+                </div>
+            </section>
+        </>
+    )
+}
+export default HeaderNoAuth
