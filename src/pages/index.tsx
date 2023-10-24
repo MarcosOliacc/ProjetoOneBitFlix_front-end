f45dbe225ee6f99d03e6de6a5d079b8e1bfe2017
Marcos Vinicius <marcosoliacc@gmail.com>
2023-10-24 11:16:21 -0300

    Initial files and configurations

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import styles from '../styles/homeNoAuth.module.scss'
+
+export const HomeNoAuth = () => {
+    return (
+        <>
+            <Head>
+                <title>Home</title>
+            </Head>
+            <main></main>
+        </>
+    )
+}
