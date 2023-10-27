ad268b0ce2278a30ea5e7c22a01df1e91236f81a
Marcos Vinicius <marcosoliacc@gmail.com>
2023-10-27 17:18:23 -0300

    Register page incremented

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,10 +1,22 @@
+import Footer from '@/components/common/footer'
 import HeaderGeneric from '@/components/common/headerGeneric'
-import '@/styles/register.module.scss'
-async function Register() {
+import styles from '@/styles/register.module.scss'
+import RegisterForm from '@/components/registerForm'
+
+
+function Register() {
     return(
         <>
-            <main>
+            <main className={styles.main}>
                 <HeaderGeneric logoUrl='/' btnUrl='/login' btnContent='Quero fazer login'/>
+                <section className={styles.sect}>
+                    <div className={styles.head}>
+                        <h2 className={styles.headTitle}>CRIE JÁ SUA CONTA!</h2>
+                        <h2 className={styles.headTitle}>É RAPIDO E FACIL.</h2>
+                    </div>
+                    <RegisterForm/>
+                </section>
+                <Footer/>
             </main>
         </>
     )
