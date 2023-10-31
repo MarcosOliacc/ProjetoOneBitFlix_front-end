df0afc3b45c8c3cae869b78bf55a23c01eef0fe9
Marcos Vinicius <marcosoliacc@gmail.com>
2023-10-31 15:24:50 -0300

    Update page.tsx

---
+++ src/app/home/page.tsx
@@ -0,0 +1,18 @@
+
+import { FeaturedSection } from "@/components/homeAuth/featuredSection"
+
+export const metadata = {
+    title: 'OneBitFlix - Home',
+    description: 'Uma plataforma streaming de cursos de programação'
+}
+
+const HomeAuth = () => {
+    return(
+        <>
+            <main>
+                <FeaturedSection/>
+            </main>
+        </>
+    )
+}
+export default HomeAuth
