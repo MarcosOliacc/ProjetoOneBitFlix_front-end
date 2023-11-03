d511be644eeea8d852bbc3cb378f492441d089a6
Marcos Vinicius <marcosoliacc@gmail.com>
2023-11-03 17:32:47 -0300

    Update index.tsx

--- src/components/common/episodeSection/index.tsx
+++ src/components/common/episodeSection/index.tsx
@@ -15,16 +15,7 @@ export default function EpisodesSection({episodes}:props) {
                     <p className={styles.listSub}>{episodes.length} episódios</p> 
                 </div>
                 
-                <div className={styles.epContent}>
-                    {episodes.map((ep:EpisodeType)=>(
-                        <div key={ep.id} className={styles.epCard}>
-                            <p className={styles.epOrder}>Episódio N° {ep.order} 
-                            <span className={styles.epLong}>
-                                {`${Math.trunc(ep.secondsLong / 60)}:${ep.secondsLong%60}`}
-                            </span></p>
-                            <h3 className={styles.epTitle}>{ep.name}</h3>
-                            <p className={styles.epDescrip}>{ep.synopsis}</p>
-                        </div>
+
                     ))}
                 </div>
                 
